95e55b68deea396a4d8355813b3c08555fb40c05
Remiphilius <remi.desmarescaux@eleve.ensai.fr>
2019-03-13 17:29:44 +0100

    Merge branch 'master' of https://github.com/LeaFgr/mml-showcase
    Conflicts:
    	org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/MmlParsingJavaTest.java
    	org.xtext.example.mml.tests/test-bin/org/xtext/example/mydsl/tests/MmlParsingJavaTest.class

--- org.xtext.example.mml.tests/mml.py
+++ org.xtext.example.mml.tests/mml.py
@@ -6,8 +6,14 @@ Y = mml_data.iloc[:,-1]
 X = mml_data.iloc[:,:-1]
 from sklearn.tree import DecisionTreeClassifier 
 classifier = DecisionTreeClassifier(max_depth=None)
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=70)
-classifier.fit(x_train,y_train)
-y_pred = classifier.predict(x_test)
-sklearn.metrics.recall_score(y_test, y_pred, labels=None, pos_label=1, average='binary', sample_weight=None)
+from sklearn.model_selection import StratifiedKFold
+skf = StratifiedKFold(n_splits=2, shuffle=True)
+for index, (train_indices, val_indices) in enumerate(skf.split(X, Y)):
+    print('Training on fold :' + str(index+1))
+    x_train, x_test = X[train_indices], X[val_indices]
+    classifier.fit(x_train,y_train)
+    y_train, y_test = Y[train_indices], Y[val_indices]
+    y_pred = classifier.predict(x_test)
+    sklearn.metrics.recall_score(y_test, y_pred, labels=None, pos_label=1, average='binary', sample_weight=None)
+    sklearn.metrics.f1_score(y_test, y_pred, labels=None, pos_label=1, average='binary', sample_weight=None)
+
--- org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/MmlParsingJavaTest.java
+++ org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/MmlParsingJavaTest.java
@@ -3,6 +3,9 @@ package org.xtext.example.mydsl.tests;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -46,12 +49,12 @@ public class MmlParsingJavaTest {
 
 	@Test
 	public void loadModel() throws Exception {
-//		MMLModel result = parseHelper.parse("datainput \"foo.csv\"\n"
-//				+ "mlframework scikit-learn\n"
-//				+ "algorithm DT\n"
-//				+ "TrainingTest { pourcentageTraining 70 }\n"
-//				+ "recall\n"
-//				+ "");
+		//		MMLModel result = parseHelper.parse("datainput \"foo.csv\"\n"
+		//				+ "mlframework scikit-learn\n"
+		//				+ "algorithm DT\n"
+		//				+ "TrainingTest { pourcentageTraining 70 }\n"
+		//				+ "recall\n"
+		//				+ "");
 		MMLModel result = parseHelper.parse("datainput \"foo.csv\" separator ,\n"
 				+ "mlframework scikit-learn\n"
 				+ "algorithm DT\n"
@@ -67,229 +70,242 @@ public class MmlParsingJavaTest {
 
 	@Test
 	public void compileDataInput() throws Exception {
-		MMLModel result = parseHelper.parse("datainput \"iris.csv\" separator ,\n"
-				+ "mlframework scikit-learn\n"
-				+ "algorithm DT\n"
-				+ "TrainingTest { pourcentageTraining 70 }\n"
-				+ "recall\n"
-				+ "");
-//		MMLModel result = parseHelper.parse("datainput \"foo.csv\"\n"
-//				+ "mlframework scikit-learn\n"
-//				+ "algorithm DT\n"
-//				+ "CrossValidation { numRepetitionCross 2 }\n"
-//				+ "recall F1\n"
-//				+ "");
-		
-//		MMLModel result = parseHelper.parse();
-		
-		DataInput dataInput = result.getInput();
-		String fileLocation = dataInput.getFilelocation();
+		//		MMLModel result = parseHelper.parse("datainput \"foo2.csv\" separator ;\n"
+		//		+ "mlframework scikit-learn\n"
+		//		+ "algorithm DT\n"
+		//		+ "TrainingTest { pourcentageTraining 70 }\n"
+		//		+ "recall\n"
+		//		+ "");
+		//MMLModel result = parseHelper.parse("datainput \"foo.csv\"\n"
+		//		+ "mlframework scikit-learn\n"
+		//		+ "algorithm DT\n"
+		//		+ "CrossValidation { numRepetitionCross 2 }\n"
+		//		+ "recall F1\n"
+		//		+ "");
+		File folder = new File("inputmml/");
+		File[] listOfFiles = folder.listFiles();
+
+		for (int f = 0; f < listOfFiles.length; f++) {
+			if (listOfFiles[f].isFile()) {
+				Scanner scanner = new Scanner(Paths.get("inputmml/" + listOfFiles[f].getName()), StandardCharsets.UTF_8.name());
+				String content = scanner.useDelimiter("\\A").next();
+				System.out.println(content);
+				scanner.close();
+				MMLModel result = parseHelper.parse(content);
+
+				//Assertions.assertNotNull(result);
+				//EList<Resource.Diagnostic> errors = result.eResource().getErrors();
+				//Assertions.assertTrue(errors.isEmpty(), "Unexpected errors");			
+				//Assertions.assertEquals("foo2.csv", result.getInput().getFilelocation());	
+
+				DataInput dataInput = result.getInput();
+				String fileLocation = dataInput.getFilelocation();
 
 
 
-		String pythonImport = "import pandas as pd\n"; 
-		CSVSeparator DEFAULT_COLUMN_SEPARATOR = CSVSeparator.COMMA; // by default
-		CSVSeparator csv_separator = DEFAULT_COLUMN_SEPARATOR;
-		CSVParsingConfiguration parsingInstruction = dataInput.getParsingInstruction();
-		if (parsingInstruction != null) {			
-			System.out.println("parsing instruction..." + parsingInstruction);
-			csv_separator = parsingInstruction.getSep();
-			if(CSVSeparator.SEMI_COLON.equals(csv_separator)) {
-				csv_separator = CSVSeparator.SEMI_COLON;
-			}
-			else if(CSVSeparator.COMMA.equals(csv_separator)) {
-				csv_separator = CSVSeparator.COMMA;
-			}
-		}
-		String csvReading = "mml_data = pd.read_csv(" + mkValueInSingleQuote(fileLocation) + ", sep=" + mkValueInSingleQuote(csv_separator.getLiteral()) + ")\n";						
-		String pandasCode = pythonImport + csvReading;
-
-
-		MLChoiceAlgorithm mlAlgorithm = result.getAlgorithm();
-		MLAlgorithm algo = mlAlgorithm.getAlgorithm();
-		String algoML="";
-
-		if (algo instanceof SVM) {
-			//apply
-			SVM svm = (SVM) algo;
-		}
-		else if(algo instanceof DT) {
-			DT dt = (DT) algo;
-			String max_depth;
-			if (dt.getMax_depth() !=0 ) {
-				max_depth = Integer.toString(dt.getMax_depth());
-			}else {
-				max_depth = "None";
-			}
-			algoML = "from sklearn.tree import DecisionTreeClassifier \nclassifier = DecisionTreeClassifier(max_depth=" +max_depth+")\n";
-		}
-		else if (algo instanceof LogisticRegression) {
-			LogisticRegression lr = (LogisticRegression) algo;
-			algoML = "from sklearn.linear_model import LogisticRegression\n";
-			algoML += "classifier = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')\n";
-			
-		}
-		else if(algo instanceof RandomForest) {
-			RandomForest rf = (RandomForest) algo;
-			algoML = "from sklearn.ensemble import RandomForestClassifier\n";
-			algoML += "classifier = RandomForestClassifier()\n";
-		}
-	
-		RFormula formula = result.getFormula();
-		String codeFormulaX ="";
-		String codeFormulaY ="";
-		if(formula == null) { //si aucune formule n'a été donnée
-			codeFormulaY = "Y = mml_data.iloc[:,-1]\n";
-			codeFormulaX = "X = mml_data.iloc[:,:-1]\n";
-		}else {//une formule a été donnée
-			FormulaItem y = formula.getPredictive();
-			XFormula x = formula.getPredictors();
-			if(y == null) { //si la predictive n'a pas été renseignée
-				codeFormulaY = "Y = mml_data.iloc[:,-1]";
-				if(x instanceof AllVariables) {//toutes les variables sont explicatives
-					codeFormulaX = "X = mml_data.iloc[:, :-1] \n";
-				}else {//si PredictorVariables
-					PredictorVariables xPred = (PredictorVariables) x;
-					//on initialise codeFormulaX
-					if(xPred.getVars().get(0).getColName() != null) {//c'est un string
-						codeFormulaX = "Y = mml_data[[" + xPred.getVars().get(0).getColName();
-					}else {//c'est un entier
-						codeFormulaX = "X = mml_data.iloc[:," + xPred.getVars().get(0).getColumn();
+				String pythonImport = "import pandas as pd\n"; 
+				CSVSeparator DEFAULT_COLUMN_SEPARATOR = CSVSeparator.COMMA; // by default
+				CSVSeparator csv_separator = DEFAULT_COLUMN_SEPARATOR;
+				CSVParsingConfiguration parsingInstruction = dataInput.getParsingInstruction();
+				if (parsingInstruction != null) {			
+					System.out.println("parsing instruction..." + parsingInstruction);
+					csv_separator = parsingInstruction.getSep();
+					if(CSVSeparator.SEMI_COLON.equals(csv_separator)) {
+						csv_separator = CSVSeparator.SEMI_COLON;
 					}
-					for(int i=1;i<xPred.getVars().size();i++) {
-						//xPred.getVars().get(i)
-						if(xPred.getVars().get(i).getColName() != null) {//c'est un string
-							codeFormulaX += ",";
-							codeFormulaX += xPred.getVars().get(i).getColName();
-						}else {//c'est un entier
-							codeFormulaX += ",";
-							codeFormulaX += xPred.getVars().get(i).getColumn();
-						}
-
+					else if(CSVSeparator.COMMA.equals(csv_separator)) {
+						csv_separator = CSVSeparator.COMMA;
 					}
-					if(xPred.getVars().get(0).getColName() != null) {//c'est un string
-						codeFormulaX += "]] \n";
-					}else {//c'est un entier
-						codeFormulaX += "] \n";
-					}
+				}
+				String csvReading = "mml_data = pd.read_csv(" + mkValueInSingleQuote(fileLocation) + ", sep=" + mkValueInSingleQuote(csv_separator.getLiteral()) + ")\n";						
+				String pandasCode = pythonImport + csvReading;
 
+
+				MLChoiceAlgorithm mlAlgorithm = result.getAlgorithm();
+				MLAlgorithm algo = mlAlgorithm.getAlgorithm();
+				String algoML="";
+
+				if (algo instanceof SVM) {
+					//apply
+					SVM svm = (SVM) algo;
+				}
+				else if(algo instanceof DT) {
+					DT dt = (DT) algo;
+					String max_depth;
+					if (dt.getMax_depth() !=0 ) {
+						max_depth = Integer.toString(dt.getMax_depth());
+					}else {
+						max_depth = "None";
+					}
+					algoML = "from sklearn.tree import DecisionTreeClassifier \nclassifier = DecisionTreeClassifier(max_depth=" +max_depth+")\n";
+				}
+				else if (algo instanceof LogisticRegression) {
+					LogisticRegression lr = (LogisticRegression) algo;
+					algoML = "from sklearn.linear_model import LogisticRegression\n";
+					algoML += "classifier = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')\n";
 
 				}
-			}else {//la prédictive a été donnée
-				if (y.getColName() != null) { //si colname est un string
-					codeFormulaY = "Y = mml_data["+y+"] \n";
-				}else { ////si colname est un int
-					codeFormulaY = "Y = mml_data.iloc[:," + y + "] \n";
-					if(x instanceof AllVariables) {
-						codeFormulaX = "X = mml_data.iloc[:, :-" + y + ":]\n";
-					} else {//PredictorVariables
+				else if(algo instanceof RandomForest) {
+					RandomForest rf = (RandomForest) algo;
+					algoML = "from sklearn.ensemble import RandomForestClassifier\n";
+					algoML += "classifier = RandomForestClassifier()\n";
+				}
 
-
-						PredictorVariables xPred = (PredictorVariables) x;
-						//on initialise codeFormulaX
-						if(xPred.getVars().get(0).getColName() != null) {//c'est un string
-							codeFormulaX = "Y = mml_data[[" + xPred.getVars().get(0).getColName();
-						}else {//c'est un entier
-							codeFormulaX = "X = mml_data.iloc[:," + xPred.getVars().get(0).getColumn();
-						}
-						for(int i=1;i<xPred.getVars().size();i++) {
-							//xPred.getVars().get(i)
-							if(xPred.getVars().get(i).getColName() != null) {//c'est un string
-								codeFormulaX += ",";
-								codeFormulaX += xPred.getVars().get(i).getColName();
+				RFormula formula = result.getFormula();
+				String codeFormulaX ="";
+				String codeFormulaY ="";
+				if(formula == null) { //si aucune formule n'a été donnée
+					codeFormulaY = "Y = mml_data.iloc[:,-1]\n";
+					codeFormulaX = "X = mml_data.iloc[:,:-1]\n";
+				}else {//une formule a été donnée
+					FormulaItem y = formula.getPredictive();
+					XFormula x = formula.getPredictors();
+					if(y == null) { //si la predictive n'a pas été renseignée
+						codeFormulaY = "Y = mml_data.iloc[:,-1]";
+						if(x instanceof AllVariables) {//toutes les variables sont explicatives
+							codeFormulaX = "X = mml_data.iloc[:, :-1] \n";
+						}else {//si PredictorVariables
+							PredictorVariables xPred = (PredictorVariables) x;
+							//on initialise codeFormulaX
+							if(xPred.getVars().get(0).getColName() != null) {//c'est un string
+								codeFormulaX = "Y = mml_data[[" + xPred.getVars().get(0).getColName();
 							}else {//c'est un entier
-								codeFormulaX += ",";
-								codeFormulaX += xPred.getVars().get(i).getColumn();
+								codeFormulaX = "X = mml_data.iloc[:," + xPred.getVars().get(0).getColumn();
+							}
+							for(int i=1;i<xPred.getVars().size();i++) {
+								//xPred.getVars().get(i)
+								if(xPred.getVars().get(i).getColName() != null) {//c'est un string
+									codeFormulaX += ",";
+									codeFormulaX += xPred.getVars().get(i).getColName();
+								}else {//c'est un entier
+									codeFormulaX += ",";
+									codeFormulaX += xPred.getVars().get(i).getColumn();
+								}
+
+							}
+							if(xPred.getVars().get(0).getColName() != null) {//c'est un string
+								codeFormulaX += "]] \n";
+							}else {//c'est un entier
+								codeFormulaX += "] \n";
+							}
+
+
+						}
+					}else {//la prédictive a été donnée
+						if (y.getColName() != null) { //si colname est un string
+							codeFormulaY = "Y = mml_data["+y+"] \n";
+						}else { ////si colname est un int
+							codeFormulaY = "Y = mml_data.iloc[:," + y + "] \n";
+							if(x instanceof AllVariables) {
+								codeFormulaX = "X = mml_data.iloc[:, :-" + y + ":]\n";
+							} else {//PredictorVariables
+
+
+								PredictorVariables xPred = (PredictorVariables) x;
+								//on initialise codeFormulaX
+								if(xPred.getVars().get(0).getColName() != null) {//c'est un string
+									codeFormulaX = "Y = mml_data[[" + xPred.getVars().get(0).getColName();
+								}else {//c'est un entier
+									codeFormulaX = "X = mml_data.iloc[:," + xPred.getVars().get(0).getColumn();
+								}
+								for(int i=1;i<xPred.getVars().size();i++) {
+									//xPred.getVars().get(i)
+									if(xPred.getVars().get(i).getColName() != null) {//c'est un string
+										codeFormulaX += ",";
+										codeFormulaX += xPred.getVars().get(i).getColName();
+									}else {//c'est un entier
+										codeFormulaX += ",";
+										codeFormulaX += xPred.getVars().get(i).getColumn();
+									}
+
+								}
+								if(xPred.getVars().get(0).getColName() != null) {//c'est un string
+									codeFormulaX += "]] \n";
+								}else {//c'est un entier
+									codeFormulaX += "] \n";
+								}
+
 							}
 
 						}
-						if(xPred.getVars().get(0).getColName() != null) {//c'est un string
-							codeFormulaX += "]] \n";
-						}else {//c'est un entier
-							codeFormulaX += "] \n";
-						}
+
+
 
 					}
+				}
+				/* LA VALIDATION */
+				Validation validation = result.getValidation();
+				StratificationMethod stratification = validation.getStratification();
+				//LA METRIQUE
+				//ValidationMetric metric = validation;
+				String metricCode ="";
+				EList<ValidationMetric> metric = validation.getMetric();
+				for(int i=0;i<metric.size();i++) {
+					if(stratification instanceof CrossValidation) {
+						metricCode +="    ";
+					}
+					if(ValidationMetric.RECALL.equals(metric.get(i))){
+						metricCode += "sklearn.metrics.recall_score(y_test, y_pred, labels=None, pos_label=1, average='binary', sample_weight=None)\n";
+					}else if(ValidationMetric.PRECISION.equals(metric.get(i))) {
+						metricCode += "sklearn.metrics.precision_score(y_test, y_pred, labels=None, pos_label=1, average='binary', sample_weight=None)\n";
+					}else if(ValidationMetric.F1.equals(metric.get(i))) {
+						metricCode += "sklearn.metrics.f1_score(y_test, y_pred, labels=None, pos_label=1, average='binary', sample_weight=None)\n";
+					}
+				}
 
+				//LA STRATIFICATION
+				String stratificationCode = "";
+				if(stratification instanceof CrossValidation) {//c'est de la crossValidation
+					CrossValidation cross = (CrossValidation) stratification;
+					int numRep = cross.getNumber();
+					//créer le x_train et le y_train
+
+					stratificationCode += "from sklearn.model_selection import StratifiedKFold\n";
+					stratificationCode += "skf = StratifiedKFold(n_splits="+numRep+", shuffle=True)\n";
+					stratificationCode +="for index, (train_indices, val_indices) in enumerate(skf.split(X, Y)):\n";
+					stratificationCode +="    print('Training on fold :' + str(index+1))\n";
+					stratificationCode +="    x_train, x_test = X[train_indices], X[val_indices]\n";
+					stratificationCode +="    classifier.fit(x_train,y_train)\n";
+					stratificationCode +="    y_train, y_test = Y[train_indices], Y[val_indices]\n";
+					stratificationCode +="    y_pred = classifier.predict(x_test)\n";
+					stratificationCode += metricCode+"\n";
+
+					/////////////REVOIR CROSS VALIDATION 
+				}else {//c'est du TrainingTest
+					TrainingTest trainTest = (TrainingTest) stratification;
+					int pourcentageTraining = trainTest.getNumber();
+					stratificationCode += "from sklearn.model_selection import train_test_split\n";
+					stratificationCode += "x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size="+pourcentageTraining+")\n";
+					stratificationCode +="classifier.fit(x_train,y_train)\n";
+					stratificationCode += "y_pred = classifier.predict(x_test)\n";
+					stratificationCode += metricCode;
 				}
 
 
+				//variable predicitive par defaut est la derniere du tableau
+				//FrameworkLang framework = mlAlgorithm.getFramework();
+				pandasCode += "\nprint (mml_data)\n"; 
+				pandasCode += codeFormulaY;
+				pandasCode += codeFormulaX;
+				pandasCode += algoML;
+				pandasCode += stratificationCode;
 
-			}
-		}
-		/* LA VALIDATION */
-		Validation validation = result.getValidation();
-		StratificationMethod stratification = validation.getStratification();
-		//LA METRIQUE
-		//ValidationMetric metric = validation;
-		String metricCode ="";
-		EList<ValidationMetric> metric = validation.getMetric();
-		for(int i=0;i<metric.size();i++) {
-			if(stratification instanceof CrossValidation) {
-				metricCode +="    ";
-			}
-			if(ValidationMetric.RECALL.equals(metric.get(i))){
-				metricCode += "sklearn.metrics.recall_score(y_test, y_pred, labels=None, pos_label=1, average='binary', sample_weight=None)\n";
-			}else if(ValidationMetric.PRECISION.equals(metric.get(i))) {
-				metricCode += "sklearn.metrics.precision_score(y_test, y_pred, labels=None, pos_label=1, average='binary', sample_weight=None)\n";
-			}else if(ValidationMetric.F1.equals(metric.get(i))) {
-				metricCode += "sklearn.metrics.f1_score(y_test, y_pred, labels=None, pos_label=1, average='binary', sample_weight=None)\n";
-			}
-		}
-		
-		//LA STRATIFICATION
-		String stratificationCode = "";
-		if(stratification instanceof CrossValidation) {//c'est de la crossValidation
-			CrossValidation cross = (CrossValidation) stratification;
-			int numRep = cross.getNumber();
-			//créer le x_train et le y_train
-			
-			stratificationCode += "from sklearn.model_selection import StratifiedKFold\n";
-			stratificationCode += "skf = StratifiedKFold(n_splits="+numRep+", shuffle=True)\n";
-			stratificationCode +="for index, (train_indices, val_indices) in enumerate(skf.split(X, Y)):\n";
-			stratificationCode +="    print('Training on fold :' + str(index+1))\n";
-			stratificationCode +="    x_train, x_test = X[train_indices], X[val_indices]\n";
-			stratificationCode +="    classifier.fit(x_train,y_train)\n";
-			stratificationCode +="    y_train, y_test = Y[train_indices], Y[val_indices]\n";
-			stratificationCode +="    y_pred = classifier.predict(x_test)\n";
-			stratificationCode += metricCode+"\n";
-			
-			/////////////REVOIR CROSS VALIDATION 
-		}else {//c'est du TrainingTest
-			TrainingTest trainTest = (TrainingTest) stratification;
-			int pourcentageTraining = trainTest.getNumber();
-			stratificationCode += "from sklearn.model_selection import train_test_split\n";
-			stratificationCode += "x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size="+pourcentageTraining+")\n";
-			stratificationCode +="classifier.fit(x_train,y_train)\n";
-			stratificationCode += "y_pred = classifier.predict(x_test)\n";
-			stratificationCode += metricCode;
-		}
+				System.out.println(pandasCode);
+				Files.write(pandasCode.getBytes(), new File("mml.py"));
+				// end of Python generation
 
 
-		//variable predicitive par defaut est la derniere du tableau
-		//FrameworkLang framework = mlAlgorithm.getFramework();
-		pandasCode += "\nprint (mml_data)\n"; 
-		pandasCode += codeFormulaY;
-		pandasCode += codeFormulaX;
-		pandasCode += algoML;
-		pandasCode += stratificationCode;
-		
-		System.out.println(pandasCode);
-		Files.write(pandasCode.getBytes(), new File("mml.py"));
-		// end of Python generation
-
-
-		/*
-		 * Calling generated Python script (basic solution through systems call)
-		 * we assume that "python" is in the path
-		 */
-		Process p = Runtime.getRuntime().exec("python3.5 mml.py");
-		BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		String line; 
-		while ((line = in.readLine()) != null) {
-			System.out.println(line);
-		}
-
+				/*
+				 * Calling generated Python script (basic solution through systems call)
+				 * we assume that "python" is in the path
+				 */
+				Process p = Runtime.getRuntime().exec("python3.5 mml.py");
+				BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				String line; 
+				while ((line = in.readLine()) != null) {
+					System.out.println(line);
+				}
+			}}
 
 
 	}
